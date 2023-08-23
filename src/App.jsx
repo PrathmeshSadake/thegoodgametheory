ad834d3541900a9b8169b3d5c4e7bbae314357c3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-08-23 10:35:42 +0530

    Fetched data using tanstack and displayed in cards with search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+import { Input } from "./components/Input";
+
 const getData = async () => {
   const { data } = await axios.get("https://api.punkapi.com/v2/beers");
   return data;
 };
 
 export default function App() {
-  const query = useQuery({ queryKey: ["data"], queryFn: getData });
-  console.log(query?.data);
+  const [searchInput, setSearchInput] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
 
+  const { isLoading, data } = useQuery({
+    queryKey: ["beers"],
+    queryFn: getData,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
-      {!query ? (
-        "Loading..."
-      ) : (
-        <div className='mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 max-w-7xl gap-5 lg:grid-cols-3'>
-          {query?.data.map((item) => (
+      <Input
+        className='max-w-3xl'
+        type='text'
+        placeholder='Search'
+        onChange={handleChange}
+        value={searchInput}
+      />
+
+      <div className='mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 max-w-7xl gap-5 lg:grid-cols-3'>
+        {data
+          ?.filter((i) => i.name.toLowerCase().match(searchInput))
+          .map((item) => (
             <div
               key={item.id}
               className='flex flex-col overflow-hidden rounded-lg shadow-lg'
@@ -58,8 +77,7 @@ export default function App() {
               </div>
             </div>
           ))}
-        </div>
-      )}
+      </div>
     </div>
   );
 }

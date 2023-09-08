0ff62cd4c6daf098c83d6ec5f6563b8ab41256f4
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-09-08 15:13:53 +0530

    Created separate card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { Input } from "./components/Input";
+import DisplayCard from "./components/DisplayCard";
 
 const getData = async () => {
   const { data } = await axios.get("https://api.punkapi.com/v2/beers");
@@ -37,35 +38,7 @@ export default function App() {
         {data
           ?.filter((i) => i.name.toLowerCase().match(searchInput))
           .map((item) => (
-            <div
-              key={item.id}
-              className='flex flex-col overflow-hidden rounded-lg shadow-lg'
-            >
-              <div className='flex-shrink-0'>
-                <img
-                  className='h-48 w-full object-cover'
-                  src={item.image_url}
-                  alt=''
-                />
-              </div>
-              <div className='flex flex-1 flex-col justify-between bg-white p-6'>
-                <div className='flex-1'>
-                  <a href={item.href} className='mt-2 block'>
-                    <p className='text-xl font-semibold text-gray-900'>
-                      {item.name}
-                    </p>
-                    <p className='mt-3 text-base text-gray-500'>
-                      {item.description}
-                    </p>
-                  </a>
-                </div>
-                <div className='mt-6 flex items-center'>
-                  <div className='flex-shrink-0'>
-                    <span className='sr-only'>{item.tagline}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <DisplayCard key={item.id} item={item} />
           ))}
       </div>
     </div>
